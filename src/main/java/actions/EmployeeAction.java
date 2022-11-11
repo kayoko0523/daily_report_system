bd0ebf80c3f4ff2125bbd793a27baf80b8fb4d0b
kayoko.tanaka <kayo.t0422@gmail.com>
2022-11-11 09:03:07 +0900

    従業員editアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -38,7 +38,9 @@ public class EmployeeAction extends ActionBase {
         //新規登録画面を表示
         forward(ForwardConst.FW_EMP_NEW);
 
-    }@Override
+    }
+
+    @Override
     public void process() throws ServletException, IOException {
 
         service = new EmployeeService();
@@ -48,6 +50,7 @@ public class EmployeeAction extends ActionBase {
 
         service.close();
     }
+
     /**
      * 新規登録を行う
      * @throws ServletException
@@ -97,6 +100,7 @@ public class EmployeeAction extends ActionBase {
 
         }
     }
+
     /**
      * 一覧画面を表示する
      * @throws ServletException
@@ -126,7 +130,52 @@ public class EmployeeAction extends ActionBase {
         //一覧画面を表示
         forward(ForwardConst.FW_EMP_INDEX);
 
-
     }
 
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException, IOException {
+
+        //idを条件に従業員データを取得する
+        EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+        if (ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+
+            //データが取得できなかった、または論理削除されている場合はエラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+            return;
+        }
+
+        putRequestScope(AttributeConst.EMPLOYEE, ev); //取得した従業員情報
+
+        //詳細画面を表示
+        forward(ForwardConst.FW_EMP_SHOW);
+    }
+    /**
+     * 編集画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void edit() throws ServletException, IOException {
+
+        //idを条件に従業員データを取得する
+        EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+        if (ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+
+            //データが取得できなかった、または論理削除されている場合はエラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+            return;
+        }
+
+        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+        putRequestScope(AttributeConst.EMPLOYEE, ev); //取得した従業員情報
+
+        //編集画面を表示する
+        forward(ForwardConst.FW_EMP_EDIT);
+
+    }
 }
