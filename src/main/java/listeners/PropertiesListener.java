88b3e52030483f188d537044739a30084484ebed
kayoko.tanaka <kayo.t0422@gmail.com>
2022-11-12 21:20:39 +0900

    訂正

--- src/main/java/listeners/PropertiesListener.java
+++ src/main/java/listeners/PropertiesListener.java
@@ -8,10 +8,12 @@ import java.util.Properties;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 @WebListener
-public class PropertiesListener {
+public class PropertiesListener implements ServletContextListener {
+
     public PropertiesListener() {
     }
 
